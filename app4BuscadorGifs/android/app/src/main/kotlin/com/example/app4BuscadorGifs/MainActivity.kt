772c0b6cdf441f44d49eb20cf82c8848a508357d
Gabriel Cardelli <gabrielcastilho.cardelli@gmail.com>
2020-06-07 23:11:48 -0300

    App 4 - Buscador de Gifs finalizado

---
+++ app4BuscadorGifs/android/app/src/main/kotlin/com/example/app4BuscadorGifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app4BuscadorGifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app4BuscadorGifs/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:app4BuscadorGifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
